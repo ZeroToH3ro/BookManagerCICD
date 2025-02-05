934a337a52b6fb2ad9e2ba0750beac080919e424
ZeroToH3ro <mtblaser2002@gmail.com>
2025-02-05 17:46:19 +0700

    change region erc

--- BookManager/obj/Debug/net9.0/BookManager.AssemblyInfo.cs
+++ BookManager/obj/Debug/net9.0/BookManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e596af8186c714877c2cf94fe36bccd1bd52587")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
